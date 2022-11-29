bcf847ddd528d703657d16e029ecd6b3e8a1879f
Stephanie Sison <sjsison27@icloud.com>
2022-11-28 19:04:48 -0800

    commit tree 7c8b760b7f1a91cdb7b62de2a96442be0efba70d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if !((self.a + self.b) > self.c) || !((self.b + self.c) > self.a) || !((self.c + self.a) > self.b)
+      raise TriangleError
+      elsif !(self.a > 0 && self.b > 0 && self.c > 0)
+        raise TriangleError
+    end
+
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+  end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
